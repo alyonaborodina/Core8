1abd13736214fbbb36e7394f153620fadb1f55f2
alyonaborodina <ms.borodinaa@mail.ru>
2017-04-17 13:33:36 +0500

    Core8

---
+++ wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Write your Javascript code.
+getDate();
+function getDate() {
+
+    var query = $.ajax("/Home/Time").done(function (result) {
+
+        $('#date').html(result);
+    });
+}
+
+setInterval(function () {
+    getDate();
+}, 1000);
